6474495e6cff0b6436ccdb93deae581f8f85d194
Lukas Krämer <>
2020-07-02 21:19:06 +0200

    Dateisuche geht - Speicherzugriff gewährt

--- app/src/main/java/com/lukaskraener/ha_analyse/MainActivity.kt
+++ app/src/main/java/com/lukaskraener/ha_analyse/MainActivity.kt
@@ -1,69 +1,84 @@
 package com.lukaskraener.ha_analyse
 
+import android.annotation.TargetApi
+import android.os.Build
 import android.os.Bundle
-import android.provider.Telephony.Carriers.PORT
-import android.webkit.WebView
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import it.sauronsoftware.ftp4j.FTPClient
-import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
+    protected fun shouldAskPermissions(): Boolean {
+        return Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1
+    }
 
+    @TargetApi(23)
+    protected fun askPermissions() {
+        val permissions = arrayOf(
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE"
+        )
+        val requestCode = 200
+        requestPermissions(permissions, requestCode)
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        if (shouldAskPermissions()) {
+            askPermissions()
+        }
         val btn_uploader = findViewById(R.id.btn_upload) as Button
         val btn_search = findViewById(R.id.btn_search) as Button
         val ftp_numername = findViewById(R.id.input_nutzername) as EditText
         val ftp_ip = findViewById(R.id.input_ip) as EditText
         val ftp_passwort = findViewById(R.id.input_passwort) as EditText
         val auswertung = findViewById(R.id.btn_auswertung) as Button
-        val webview =findViewById(R.id.wb1) as WebView
         val uploaderfertiganzeige = findViewById<TextView>(R.id.tv_uplaoder_fertig)
+        var anzeige_oben = findViewById<TextView>(R.id.tv_sache_res)
 
         btn_uploader.setOnClickListener() {
             uploaderfertiganzeige.text = "gestartet"
-            val meldung: String= ftp_uplaoder(ftp_ip, ftp_numername, ftp_passwort)
+            val meldung: String = ftp_uplaoder(ftp_ip, ftp_numername, ftp_passwort)
             uploaderfertiganzeige.text = meldung
 
         }
-        auswertung.setOnClickListener{
-            webview.loadUrl("https://yarix.ddns.net")
+        auswertung.setOnClickListener {
+            //webview.loadUrl("https://yarix.ddns.net")
         }
-        btn_search.setOnClickListener{
+        btn_search.setOnClickListener {
+            anzeige_oben.text = read_files.reader().toString()
+
+        }
+    }
+
+        private fun ftp_uplaoder(
+            adress: EditText,
+            nutzername: EditText,
+            passwort: EditText
+        ): String {
+
+
+            val user: String = nutzername.text.toString()
+            val pass: String = passwort.text.toString()
+            val ip: String = adress.text.toString()
+
+
+            return try {
+                smb.reader(user, pass, ip)
+                ("erfolgreich")
+
+            } catch (e: Exception) {
+                ("Fehler")
+            }
+
 
         }
 
     }
 
-    private fun ftp_uplaoder( adress: EditText, nutzername: EditText, passwort: EditText): String {
-
-
-        val user: String = nutzername.text.toString()
-        val pass: String = passwort.text.toString()
-        val ip: String = adress.text.toString()
-
-
-        try {
-            smb.init(user,pass,ip)
-            smb.reader(user,pass,ip)
-            return ("erfolgreich")
-
-        } catch (e: Exception) {
-            return ("Fehler")
-        }
-
-
-
-    }
-
-}
 
 
---
+++ app/src/main/java/com/lukaskraener/ha_analyse/smb.kt
@@ -0,0 +1,46 @@
+package com.lukaskraener.ha_analyse
+
+import com.hierynomus.smbj.SMBClient
+import com.hierynomus.smbj.auth.AuthenticationContext
+import com.hierynomus.smbj.session.Session
+import com.hierynomus.smbj.share.DiskShare
+import java.io.IOException
+
+object smb {
+    var session: Session? = null
+    fun init(
+        username: String?,
+        passwort: String,
+        ip: String?
+    ): Session? {
+        val client = SMBClient()
+        try {
+            client.connect(ip).use { connection ->
+                val ac =
+                    AuthenticationContext(username, passwort.toCharArray(), ip)
+                session = connection.authenticate(ac)
+                return session
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return null
+        }
+    }
+
+    @Throws(IOException::class)
+    fun reader(username: String?, passwort: String, ip: String?): Int {
+        try{
+
+        } catch (e: IOException) {
+            e.message
+            return 1
+        }
+    }
+
+    fun delete(username: String?, passwort: String, ip: String?): Int {
+        session = init(username, passwort, ip)
+        val share = session!!.connectShare("SHARENAME") as DiskShare
+        run { share.rm("FILE") }
+        return 0
+    }
+}
